4bbb56ffce8a6091f08113c9718ca1f24db60eea
Juda Escalera Flores <juda.escaleraflores@credencialprocessing.com>
2020-10-25 16:02:54 -0600

    initial commit

---
+++ src/main/java/com/example/demolibraryremote/InitialHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.demolibraryremote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_home);
+    }
+}
